90e7e01e31028c7f86ca4ce124ed765d30bf02ee
rishaaanp <147536759+rishaaanp@users.noreply.github.com>
2025-03-28 17:24:16 +0530

    Delete Python/5) reverse of a number.py

--- Python/5) reverse of a number.py
+++
@@ -1,9 +0,0 @@
-x=int(input("Enter the number"))
-reverse=0
-y=x
-while x>0:
-    reverse=reverse*10
-    reverse=reverse+x%10
-    x=x//10
-print("Given number is",y)
-print("Reverse of the number is",reverse)
